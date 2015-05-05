e4fd26c9d3d9a80dd234d219f73c09d9d299554e
Alexey Marchenko <alexey.marchenko@lge.com>
2015-05-05 16:58:07 +0300

    fix generate_strings

--- src/common/generate_strings.h
+++ src/common/generate_strings.h
@@ -11,24 +11,32 @@
 * Example:
 * Input : a?b?c?
 * Output: a0b0c0, a0b0c1, a0b1c0, a0b1c1, a1b0c0, a1b0c1, a1b1c0, a1b1c1.
+*
+* Created on May 4, 2015
+* By Alex
 */
 
 namespace generate_strings {
 
-    void ReplaceByZerosOnes(std::string str, unsigned int pos) {
-    if (pos == str.size()) std::cout << str << std::endl;
+void ReplaceByZerosOnes(std::string str, unsigned int pos) {
+
+    if (pos == str.size()) {
+        std::cout << str << std::endl;
+        return;
+    }
 
     if (str[pos] == '?') {
-        std::string prefix = str.substr(0, pos - 1);
+        std::string prefix = str.substr(0, pos);
         std::string suffix = str.substr(pos + 1);
         ReplaceByZerosOnes(prefix + '0' + suffix, pos + 1);
         ReplaceByZerosOnes(prefix + '1' + suffix, pos + 1);
+    } else {
+        ReplaceByZerosOnes(str, pos + 1);
     }
-
 }
 
 int test() {
-    std::string test = "acb?cde?";
+    std::string test = "abc?def?gh?";
     ReplaceByZerosOnes(test, 0);
     return 1;
 }
